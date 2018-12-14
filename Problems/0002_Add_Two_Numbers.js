80fb1431cb9455873ef968fbf58527767683f585
Kris <Kris@Kriss-MacBook-Pro-2.local>
2018-12-14 02:54:43 -0800

    0002_Add_Two_Numbers.js unresolved

---
+++ Problems/0002_Add_Two_Numbers.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+
+var addTwoNumbers = function(l1, l2) {
+
+
+    var myVal = l1.val+l2.val;
+    var myNode = ListNode(myVal);
+    console.log(myNode);
+    console.log(l2);
+    return 2;
+
+
+
+};
