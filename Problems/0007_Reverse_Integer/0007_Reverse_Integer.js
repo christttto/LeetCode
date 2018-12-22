c3381a73a322ae73f1f7a4a2bc6cb095fe4a42f1
Kris <Kris@Kriss-MacBook-Pro-2.local>
2018-12-21 23:34:41 -0800

    0007 works even for the integer bigger than 32bit signed integer

--- Problems/0007_Reverse_Integer/0007_Reverse_Integer.js
+++ Problems/0007_Reverse_Integer/0007_Reverse_Integer.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 var reverse = function(x) {
-    
+    //understand 32-bit signed integer, reverse digits of an integer
+    //wait no need not really
+    var result = 0;
+    var i = 1;
+    var mySign = Math.sign(x);
+    x = x * mySign;
+    while(x>0){
+        result = (result*10)+(x%10)*i;
+        x = parseInt(x/10);
+    }
+    return result*mySign;
+
 };
