d273f05fa0f86767acf6bb5c911825d329a55479
Kris <christttto@berkeley.edu>
2018-12-27 20:15:44 -0800

    0014 works

--- Problems/0014_Longest_Common_Prefix/0014_Longest_Common_Prefix.js
+++ Problems/0014_Longest_Common_Prefix/0014_Longest_Common_Prefix.js
@@ -3,5 +3,24 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    
+    //if strs is empty, need to cover that
+    if(strs.length==0){
+        return "";
+    }
+    var prefix=strs[0];//default
+    var i;
+    for(i=1;i<strs.length;i++){
+        var curWord=strs[i];
+        var j;
+        if(curWord.length<prefix.length){
+            console.log("prefix: "+prefix+" curWord:"+curWord);
+            prefix = prefix.substring(0,curWord.length);
+        }
+        for(j=0;j<curWord.length;j++){
+            if(!(curWord.charAt(j)==prefix.charAt(j))){
+                prefix = prefix.substring(0,j);
+            }
+        }
+    }
+    return prefix;
 };
