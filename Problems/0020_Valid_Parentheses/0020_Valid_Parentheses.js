64cfffb83705eef18381803e42b28a12ac4b4f9f
Kris <christttto@berkeley.edu>
2018-12-28 15:40:59 -0800

    0020 draft

--- Problems/0020_Valid_Parentheses/0020_Valid_Parentheses.js
+++ Problems/0020_Valid_Parentheses/0020_Valid_Parentheses.js
@@ -3,5 +3,29 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    var isValidHelper = function(s,prevBracket){//helper for previous
+        if(s.length>0){
+            var curBracket = s.charAt(0);
+            console.log(curBracket);
+            if(curBracket==")"){
+                if(prevBracket!="("){return false;}
+                else{return isValidHelper(s.substring(1,s.length-1));}
+            }
+            else if(curBracket=="}"){
+                if(prevBracket!="{"){return false;}
+                else{return isValidHelper(s.substring(1,s.length-1));}
+            }
+            else if(curBracket=="]"){
+                if(prevBracket!="["){return false;}
+                else{return isValidHelper(s.substring(1,s.length-1));}
+            }
+            else{
+                return isValidHelper(s.substring(1,s.length-1),curBracket);
+            }
+            
+        }
+        return true;
+        //if s is empty, return nothing
+    }
+    return isValidHelper(s,"");
 };
