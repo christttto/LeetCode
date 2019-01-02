105db2050f09577e6965d9e532e578825fb91116
Kris <christttto@berkeley.edu>
2019-01-01 23:30:02 -0800

    0035 done

--- Problems/0035_Search_Insert_Position/0035_Search_Insert_Position.js
+++ Problems/0035_Search_Insert_Position/0035_Search_Insert_Position.js
@@ -4,5 +4,28 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
+    /*
+    if(nums.indexOf(target)!=-1){
+        return nums.indexOf(target);
+    }
+    */
+    if(nums.length==0){//empty
+        return 0;
+    }
+    else if(nums[0]==target){//found it
+        return 0;
+    }
+    else if(nums[Math.floor(nums.length/2)]==target){//half and target is smaller
+        return Math.floor(nums.length/2);
+    }
+    else if(nums[Math.floor(nums.length/2)]>target){//half and target is smaller
+        return searchInsert(nums.slice(0,nums.length/2),target);
+    }
+    else if(nums[Math.floor(nums.length/2)]<target){//half and target is bigger
+        return (1+Math.floor(nums.length/2))+searchInsert(nums.slice((nums.length/2)+1,nums.length),target);
+    }
+    console.log(nums.length/2);
+    console.log(nums[nums.length/2]+"adf"+target);
+    
     
 };
