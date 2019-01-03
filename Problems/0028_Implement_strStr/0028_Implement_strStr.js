53b65e8ac4b5a3f6d81a080a38c5218c487e7d38
Kris <christttto@berkeley.edu>
2019-01-03 00:56:34 -0800

    0028

--- Problems/0028_Implement_strStr/0028_Implement_strStr.js
+++ Problems/0028_Implement_strStr/0028_Implement_strStr.js
@@ -4,5 +4,6 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    
+    //loops that start with less number of character left than needle is unnecessary
+    return haystack.search(needle);
 };
