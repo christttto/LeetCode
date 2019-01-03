cb6eaeb6d45aeea2d1eda34f10d30be0eb10bc58
Kris <christttto@berkeley.edu>
2019-01-03 01:22:08 -0800

    0038

--- Problems/0038_Count_And_Say/0038_Count_And_Say.js
+++ Problems/0038_Count_And_Say/0038_Count_And_Say.js
@@ -3,5 +3,17 @@
  * @return {string}
  */
 var countAndSay = function(n) {
+    var returnStr="1";
+    var i;
+    var currentChar;
+    var currentCount=1;
+    for(i=0;i<returnStr.length-1;i++){//need to think about case with length 1
+        currentChar=returnStr[i];
+        while(currentChar=)
+        if(returnStr[i]==returnStr[i+1]){
+            currentCount++;
+        }
+    }
+    
     
 };
