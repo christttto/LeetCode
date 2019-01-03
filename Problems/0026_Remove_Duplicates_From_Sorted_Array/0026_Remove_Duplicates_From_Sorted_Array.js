9eceba253bcb884b9ccf4dc2939bbfe4d1ef333b
Kris <christttto@berkeley.edu>
2019-01-03 00:38:18 -0800

    0026 draft

--- Problems/0026_Remove_Duplicates_From_Sorted_Array/0026_Remove_Duplicates_From_Sorted_Array.js
+++ Problems/0026_Remove_Duplicates_From_Sorted_Array/0026_Remove_Duplicates_From_Sorted_Array.js
@@ -3,5 +3,17 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    
+    var originalLength=nums.length;
+    var i;
+    for(i=0;i<nums.length-1;i++){//loop length-1, so it use the next value
+        //if the length was zero, returned this
+        //console.log(nums);
+        //console.log(nums[i]+"asdf"+nums[i+1]);
+        if(nums[i]==nums[i+1]){
+            nums.splice(i+1,1);
+            i=i-1;
+            //console.log(nums+"insideif");
+        }
+    }
+    return nums.length;
 };
