1539142f7b17f7d0b41f906110756f83586e6afb
Kris <christttto@berkeley.edu>
2019-01-03 00:46:38 -0800

    0027_Remove_Element

---
+++ Problems/0027_Remove_Element/0027_Remove_Element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    //
+    var originalLength=nums.length;
+    var i;
+    for(i=0;i<nums.length;i++){
+        if(nums[i]==val){
+            nums.splice(i,1);
+            i=i-1;
+        }
+    }
+    return nums.length;
+};
