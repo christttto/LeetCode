fced5073b86e248bf786a0c05dff56b01fae3730
Kris <christttto@berkeley.edu>
2019-01-18 12:08:03 -0800

    0053 draft

--- Problems/0053_Maximum_Subarray/0053_Maximum_Subarray.js
+++ Problems/0053_Maximum_Subarray/0053_Maximum_Subarray.js
@@ -3,5 +3,24 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    
+    var max=nums[0];
+    var i;
+    for(i=0;i<nums.length;i++){
+        console.log(i<nums.length);
+        console.log("=====")
+        var k=i;
+        var total=0;
+        console.log(k);
+        console.log(nums.length);
+        console.log(k<nums.length);
+        console.log("secondloop");
+        for(k=i;k<nums.lenth;k++){
+            console.log("sanity check");
+            total= total+nums[k];
+            if(total>max){
+                max=total;
+            }
+        }
+    }
+    return max;
 };
