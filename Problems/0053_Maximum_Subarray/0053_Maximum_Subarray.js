b36ae6a98f16495b60bfc1ed64911c10b5f62a9a
Kris <christttto@berkeley.edu>
2019-01-18 12:15:36 -0800

    0053 done

--- Problems/0053_Maximum_Subarray/0053_Maximum_Subarray.js
+++ Problems/0053_Maximum_Subarray/0053_Maximum_Subarray.js
@@ -6,16 +6,10 @@ var maxSubArray = function(nums) {
     var max=nums[0];
     var i;
     for(i=0;i<nums.length;i++){
-        console.log(i<nums.length);
-        console.log("=====")
-        var k=i;
+        var k;
         var total=0;
-        console.log(k);
-        console.log(nums.length);
-        console.log(k<nums.length);
         console.log("secondloop");
-        for(k=i;k<nums.lenth;k++){
-            console.log("sanity check");
+        for(k=i;k<nums.length;k++){
             total= total+nums[k];
             if(total>max){
                 max=total;
