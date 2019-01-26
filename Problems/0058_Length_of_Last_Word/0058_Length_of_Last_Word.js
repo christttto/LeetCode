c2b3c6a541acfe169bb729cdb093af950d269e79
Kris <christttto@berkeley.edu>
2019-01-25 17:55:08 -0800

    0058

--- Problems/0058_Length_of_Last_Word/0058_Length_of_Last_Word.js
+++ Problems/0058_Length_of_Last_Word/0058_Length_of_Last_Word.js
@@ -6,7 +6,6 @@ var lengthOfLastWord = function(s) {
     s=s.trim();
     let count=0;
     let i;
-    console.log(s);
     for(i=s.length-1;0<=i;i--){
         if(s[i]!=" "){
             count=count+1;
