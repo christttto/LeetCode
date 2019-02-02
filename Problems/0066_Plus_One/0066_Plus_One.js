48cd84002f4095f0585e848207fc742f129d6b8d
Kris <christttto@berkeley.edu>
2019-02-02 10:37:14 -0800

    0066

--- Problems/0066_Plus_One/0066_Plus_One.js
+++ Problems/0066_Plus_One/0066_Plus_One.js
@@ -4,4 +4,21 @@
  */
 var plusOne = function(digits) {
     
+    let last_number = digits[digits.length-1];
+    let i=digits.length;
+    while(i>0){//[9,9,9] [1,0,0,0] 
+        if(digits[i-1]==9){
+            digits[i-1]=0;
+            if(i==1){
+                console.log(digits);
+                digits.splice(0,0,1);
+            }
+            i--;
+        }
+        else{
+            digits[i-1]++;
+            break;
+        }
+    }
+    return digits
 };
