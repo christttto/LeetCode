4c08c0dd1e601bda0e193a6a0456c8fcf722a11f
Kris <christttto@berkeley.edu>
2019-02-07 12:50:18 -0800

    0067

--- Problems/0067_Add_Binary/0067_Add_Binary.js
+++ Problems/0067_Add_Binary/0067_Add_Binary.js
@@ -5,6 +5,7 @@
  */
 var addBinary = function(a, b) {
     //from reverse, keep iterating until one of the input runs out
+    /*
     let longB
     let shortB
     if(a.length>b.length){
@@ -15,10 +16,22 @@ var addBinary = function(a, b) {
         longB = b
         shortB = a
     }
-    let i
-    let addDigit = false;
-    for(i=longB.length-1;i>=0;i--){
-        longB.charAt(i)
+    let i=shortB.length-1
+    let j
+    let addDigit = false
+    for(i=longB.length-1.length-1;i>=0;i--){
+        if(addDigit){
+            if(longB[i]==1&&shortB[j]==1)
+        }
     }
+    */
+    console.log(parseInt(a,2))
+    console.log(parseInt(b,2))
+    let intTotal=parseInt(a,2)+parseInt(b,2)
+    console.log(intTotal)
+    
+    return intTotal.toString(2)
+    
+    
     
 };
