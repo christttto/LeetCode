b037bc6a5a9e82aedd46f60be5d29a946a4eb87c
Kris <christttto@berkeley.edu>
2019-02-09 20:58:45 -0800

    0070

--- Problems/0070_Climbing_Stairs/0070_Climbing_Stairs.js
+++ Problems/0070_Climbing_Stairs/0070_Climbing_Stairs.js
@@ -3,5 +3,19 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    
+    switch(n){
+        case 0: return 0
+        case 1: return 1
+        case 2: return 2
+    }
+    let i = 0
+    let j = 1
+    let k = 2
+    for(i=0;i<n-2;i++){
+        j+=k
+        let l=k
+        k=j
+        j=l
+    }
+    return k
 };
