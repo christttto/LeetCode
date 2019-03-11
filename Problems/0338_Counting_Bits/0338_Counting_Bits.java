df9bea1ed2e86e4ab4236c11f16f324d40d70e11
Kris <christttto@berkeley.edu>
2019-03-11 14:17:21 -0700

    know the pattern, cannot implement

---
+++ Problems/0338_Counting_Bits/0338_Counting_Bits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] countBits(int num) {
+        //so do the easy way first
+        //turn the number binary, count all the 1
+        //
+        int returnList = new ArrayList<Integer>();
+        returnList.add(0);
+        if(num==0) return returnList;
+        returnList.add(1);
+        if(num==1) return returnList;
+        int i=1;
+        while(i<num){
+            returnList.add(1+)
+        }
+        
+        
+    }
+}
