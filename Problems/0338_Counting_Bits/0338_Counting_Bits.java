48348f986ee27bc852aa440e008bc91d34fe0b88
Kris <christttto@berkeley.edu>
2019-03-11 14:34:54 -0700

    my own write up

--- Problems/0338_Counting_Bits/0338_Counting_Bits.java
+++ Problems/0338_Counting_Bits/0338_Counting_Bits.java
@@ -1,31 +1,16 @@
 class Solution {
     public int[] countBits(int num) {
-        int[] ret = new int[num+1];//make a list of integer with right length
-        ret[0] = 0;//set the base case
-        int pow = 1;//how much it should be powered by
-        for(int i = 1, t = 0; i <= num; i++, t++) { //set up the iterator. t is for?
-            if(i == pow) { //
-                pow *= 2; // everytime it reaches the double point, reset and make the range double.
-                t = 0;
-            }
-            // going normal pace = at t + 1
-            ret[i] = ret[t] + 1;
-        }
-        return ret;
-    }
-}
-
-class Solution{
-    public int[] countBits(int num){
         int[] returnList = new int[num+1];
         returnList[0]=0;
+        //2 pointers
+        //needs breakpoint to know when it should be taken back to start
         int addDigit = 1;
-        for(int i=1,int t=0;i<=num;i++,t++){
-            if(i==addDigit){
-                addDigit *=2;
-                t=0;
+        for(int i=1,c=0;i<=num;i++,c++){
+            if(c==addDigit){
+                addDigit*=2;
+                c=0;
             }
-            returnList[i]=returnList[t]+1;
+            returnList[i]=returnList[c]+1;
         }
         return returnList;
     }
