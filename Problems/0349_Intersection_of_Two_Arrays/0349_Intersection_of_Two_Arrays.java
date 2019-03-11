514c9e6caecc94b3554208a48c7fbbbcfe36f12e
Kris <christttto@berkeley.edu>
2019-03-11 16:25:36 -0700

    349 re-did without looking at the answer

--- Problems/0349_Intersection_of_Two_Arrays/0349_Intersection_of_Two_Arrays.java
+++ Problems/0349_Intersection_of_Two_Arrays/0349_Intersection_of_Two_Arrays.java
@@ -1,14 +1,18 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        //sort, remove duplicates
+        //so no need to sort
         HashSet<Integer> set1 = new HashSet<Integer>();
-        for(Integer i: nums1)set1.add(i);
+        for(int n: nums1) set1.add(n);
         HashSet<Integer> set2 = new HashSet<Integer>();
-        for(Integer i: nums2)set2.add(i);
+        for(int n: nums2) set2.add(n);
+        //make two sets for nums1,2
         set1.retainAll(set2);
-        int[] returnList = new int[set1.size()];
-        int i=0;
-        for(int n: set1)returnList[i++]=n;
-        return returnList;
+        //key function of hashset, retainAll. does intersection
+        
+        //done with making, just reformatting to be array
+        int[] returnArray = new int[set1.size()];//make array of setsize.
+        int j=0;
+        for(int n: set1)returnArray[j++] = n;
+        return returnArray;
     }
 }
