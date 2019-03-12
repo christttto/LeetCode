fb650110671d78250de9dee0dc6b30390f83e51e
Kris <christttto@berkeley.edu>
2019-03-11 18:55:14 -0700

    0326 good approach, saw the answer

--- Problems/0326_Power_of_Three/0326_Power_of_Three.java
+++ Problems/0326_Power_of_Three/0326_Power_of_Three.java
@@ -1,19 +1,5 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        //first, with the loop
-        int poweredThree=3;
-        while(poweredThree<=n){
-            System.out.println(poweredThree);
-            if(poweredThree==n)return true;
-            poweredThree *=3;
-        }
-        return false;
-        //3, 9, 27, 81, 243, 729, 7
-        //3, 9, 7, 1, 3, 9, 7
-        //sum of the digits divides to be divisible by 3
-        //maybe 9?
-        //
-        //what about 33, 45, 63
-        //657=
+        return (n>0 && Math.pow(3,19)%n==0);
     }
 }
