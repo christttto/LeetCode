28c51fa6e19fb4c2f5ab889223cc2619bf4a3e8e
Kris <christttto@berkeley.edu>
2019-03-11 22:57:39 -0700

    0001 buggyyyyy

--- Problems/0001_Two_Sum/0001_Two_Sum.java
+++ Problems/0001_Two_Sum/0001_Two_Sum.java
@@ -8,7 +8,9 @@
  */
 class Solution {
         ListNode addTwoNumbersHelper(ListNode l1, ListNode l2, int carry){
+        
         if(l1==null&&l2==null){
+            System.out.println("lastDigit"+carry);
             if(carry==1){
                 return new ListNode(1);
             }
@@ -21,35 +23,30 @@ class Solution {
             if(carry==1){
                 l2.val++;
             }
-            return l2;
+            ListNode myNode = addTwoNumbersHelper(null,l2.next,0);
+            return myNode;
         }
         else if(l2==null){
             if(carry==1){
                 l1.val++;
             }
-            return l1;
+            ListNode myNode = addTwoNumbersHelper(l1.next,null,0);
+            return myNode;
         }
         else{
-            int myValue = (l1.val+l2.val+carry)%10;
-            System.out.println(myValue);
             System.out.println("sanity check:"+15%10);
             int myCarry=0;
             if((l1.val+l2.val+carry)>=10){
                 System.out.println(l1.val+l2.val+carry);
                 myCarry++;
             }
-            ListNode myNode = new ListNode(myValue);
+            System.out.println("myCarry"+myCarry);
+            ListNode myNode = new ListNode((l1.val+l2.val+carry)%10);
             myNode.next = addTwoNumbersHelper(l1.next,l2.next,myCarry);
             return myNode;
         }
     }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        //recursion
-        //grabs the myNode(l1.val+l2.val%10 )
-        //for the next integer, add roundDown(l1.val+l2.val/10)
-        
-        //if both node null, return nothing
-        //if one node null, return the other node
         return addTwoNumbersHelper(l1,l2,0);
     }
 }
