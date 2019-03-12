2e75d1643934e31a08e71c06c4d7b158e8ea70af
Kris <christttto@berkeley.edu>
2019-03-11 23:09:06 -0700

    buggygygygygyygy

--- Problems/0001_Two_Sum/0001_Two_Sum.java
+++ Problems/0001_Two_Sum/0001_Two_Sum.java
@@ -7,40 +7,44 @@
  * }
  */
 class Solution {
-        ListNode addTwoNumbersHelper(ListNode l1, ListNode l2, int carry){
-        
+    private ListNode addTwoNumbersHelper(ListNode l1, ListNode l2, int carry){
         if(l1==null&&l2==null){
-            System.out.println("lastDigit"+carry);
             if(carry==1){
                 return new ListNode(1);
             }
             else{
                 return null;
             }
-
         }
         else if(l1==null){
+            int myCarry=0;
             if(carry==1){
                 l2.val++;
+                if(carry+l2.val>=10){
+                    myCarry++;
+                }
             }
-            ListNode myNode = addTwoNumbersHelper(null,l2.next,0);
+            ListNode myNode = new ListNode((carry+l2.val)%10);
+            myNode.next = addTwoNumbersHelper(null,l2.next,myCarry);
             return myNode;
         }
         else if(l2==null){
+            int myCarry=0;
             if(carry==1){
                 l1.val++;
+                if(carry+l1.val>=10){
+                    myCarry++;
+                }
             }
-            ListNode myNode = addTwoNumbersHelper(l1.next,null,0);
+            ListNode myNode = new ListNode((carry+l1.val)%10);
+            myNode.next = addTwoNumbersHelper(l1.next,null,myCarry);
             return myNode;
         }
         else{
-            System.out.println("sanity check:"+15%10);
             int myCarry=0;
             if((l1.val+l2.val+carry)>=10){
-                System.out.println(l1.val+l2.val+carry);
                 myCarry++;
             }
-            System.out.println("myCarry"+myCarry);
             ListNode myNode = new ListNode((l1.val+l2.val+carry)%10);
             myNode.next = addTwoNumbersHelper(l1.next,l2.next,myCarry);
             return myNode;
