257810605be095453468d8e324da929441d26b34
Kris <christttto@berkeley.edu>
2019-03-13 13:00:25 -0700

    0020 solved agian without looking at the solution

--- Problems/0011_Container_With_Most_Water/0011_Container_With_Most_Water.java
+++ Problems/0011_Container_With_Most_Water/0011_Container_With_Most_Water.java
@@ -1,29 +1,11 @@
 class Solution {
-    public int maxAreaHelper(int[] height) {
-        //naive brute force way would be calculating every permutation
-            //Min(a,b)*(ib - ia)
-            //better way would be saving only the best
-            //time complexity of n^2
-            //we can rule out a if the shorter current c is taller.
-        //two pointer approach
-        int bestVolume=Math.min(height[0],height[1]);
-        for(int i=0;i<height.length-1;i++){
-            for(int j=i+1;j<height.length;j++){
-                int myVolume=Math.min(height[i],height[j])*(j-i);
-                if(myVolume>bestVolume){
-                    bestVolume=myVolume;
-                }
-            }
-        }
-        return bestVolume;
-    }
     public int maxArea(int[] height){
-        int a=0,b=height.length-1,bestVolume=0;
+        int maxArea=0,a=0,b=height.length-1;
         while(a<b){
-            bestVolume=Math.max(bestVolume,(Math.min(height[a],height[b])*(b-a)));
+            maxArea=Math.max(maxArea,Math.min(height[a],height[b])*(b-a));
             if(height[a]<height[b]) a++;
-            else  b--;
+            else b--;
         }
-        return bestVolume;      
+        return maxArea;
     }
 }
