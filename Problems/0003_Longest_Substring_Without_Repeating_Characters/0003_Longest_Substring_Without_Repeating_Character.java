88d169d40b4274bce47bd56a54de3df729493349
Kris <christttto@berkeley.edu>
2019-03-13 16:38:36 -0700

    0020 understand the answer

--- Problems/0003_Longest_Substring_Without_Repeating_Characters/0003_Longest_Substring_Without_Repeating_Character.java
+++ Problems/0003_Longest_Substring_Without_Repeating_Characters/0003_Longest_Substring_Without_Repeating_Character.java
@@ -1,17 +1,19 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Set set = new HashSet<Character>();
+        Set set = new HashSet<Character>();//set up a set
         int longestSubstring=0;
         int i=0,j=0;
-        while(i<s.length()&&j<s.length()){
-            if(!set.contains(s.charAt(j))){
-                set.add(s.charAt(j++));
-                longestSubstring = Math.max(longestSubstring,j-i);
+        while(i<s.length()&&j<s.length()){//while we iterate on s
+            if(!set.contains(s.charAt(j))){//if its a new character
+                set.add(s.charAt(j++));//add to the set
+                longestSubstring = Math.max(longestSubstring,j-i);//see if best record
             }
-            else{
-                set.remove(s.charAt(i++));
+            else{//oh no. I saw this before.
+                //because we have been checking every single
+                //it must be the first one that overlaps
+                set.remove(s.charAt(i++));//remove the first character,increment pointer
             }
-        }
+        }//once we have finished, return
         return longestSubstring;
     }
 }
