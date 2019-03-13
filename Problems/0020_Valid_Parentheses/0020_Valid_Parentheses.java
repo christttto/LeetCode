2545239e9bbb91c8ec85760d324455333bb5bc92
Kris <christttto@berkeley.edu>
2019-03-13 14:27:25 -0700

    0020 should remember taht I have to check if stack is empty. char c=sStack.empty()?'#':sStack.pop();

--- Problems/0020_Valid_Parentheses/0020_Valid_Parentheses.java
+++ Problems/0020_Valid_Parentheses/0020_Valid_Parentheses.java
@@ -1,5 +1,23 @@
 class Solution {
     public boolean isValid(String s) {
-
+        Stack<Character> sStack = new Stack<Character>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==')'){
+                char c=sStack.empty()?'#':sStack.pop();
+                if(c!='(') return false;
+            }
+            else if(s.charAt(i)=='}'){
+                char c=sStack.empty()?'#':sStack.pop();
+                if(c!='{') return false;
+            }
+            else if(s.charAt(i)==']'){
+                char c=sStack.empty()?'#':sStack.pop();
+                if(c!='[') return false;
+            }
+            else{
+                sStack.push(s.charAt(i));
+            }
+        }
+        return sStack.empty();
     }
 }
