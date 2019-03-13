940faf901d488e4bf007d03dbf517f8b2eb50139
Kris <christttto@berkeley.edu>
2019-03-12 19:28:28 -0700

    0020 re-did the problem without looking at the answer

--- Problems/0020_Valid_Parentheses/0020_Valid_Parentheses.java
+++ Problems/0020_Valid_Parentheses/0020_Valid_Parentheses.java
@@ -2,23 +2,23 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> sStack = new Stack<Character>();
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(c==')'){
-                char top = sStack.empty()?'#':sStack.pop();
-                if(top!='(')return false;
+            char myC = s.charAt(i);
+            if(myC==')'){
+                char c=sStack.empty()?'#':sStack.pop();
+                if(c!='(')return false;
             }
-            else if(c=='}'){
-                char top = sStack.empty()?'#':sStack.pop();
-                if(top!='{')return false;
+            else if(myC=='}'){
+                char c=sStack.empty()?'#':sStack.pop();
+                if(c!='{')return false;
             }
-            else if(c==']'){
-                char top = sStack.empty()?'#':sStack.pop();
-                if(top!='[')return false;
+            else if(myC==']'){
+                char c=sStack.empty()?'#':sStack.pop();
+                if(c!='[')return false;
             }
             else{
-                sStack.push(c);
+                sStack.push(myC);
             }
         }
-        return sStack.isEmpty();
+        return sStack.empty();
     }
 }
