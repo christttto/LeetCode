a82201aee70642777d6c46822b75a6922e4fea95
Kris <christttto@berkeley.edu>
2019-03-13 17:17:35 -0700

    0003 comeback to understand the optimized window solution

--- Problems/0003_Longest_Substring_Without_Repeating_Characters/0003_Longest_Substring_Without_Repeating_Character.java
+++ Problems/0003_Longest_Substring_Without_Repeating_Characters/0003_Longest_Substring_Without_Repeating_Character.java
@@ -1,14 +1,14 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Set set = new HashSet<Character>();
+        Set sSet = new HashSet<Character>();
         int answer=0,i=0,j=0;
-        while(i<s.length()&&j<s.length()){
-            if(!set.contains(s.charAt(j))){
-                set.add(s.charAt(j++));
+        while(j<s.length()){
+            if(!sSet.contains(s.charAt(j))){//if it is a new character
+                sSet.add(s.charAt(j++));
                 answer = Math.max(answer,j-i);
             }
-            else{
-                set.remove(s.charAt(i++));
+            else{//something already seen
+                sSet.remove(s.charAt(i++));
             }
         }
         return answer;
