8e970d28d2b6df2519a4e0543f8be7c424491f4f
Kris <christttto@berkeley.edu>
2019-03-18 22:21:55 -0700

    0507 looked up fast way to find all the divisors

--- Problems/0507_Perfect_Number/0507_Perfect_Number.java
+++ Problems/0507_Perfect_Number/0507_Perfect_Number.java
@@ -1,5 +1,27 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
+        //equal to the sum of all its positive divisor
+        //find all divisors
+        //try dividing by 1-num, takes O(n)
+        //28 = 2 * 2 * 7
+        //1,2,4,7,14 = permutation of all the combo
+        //find all the prime number that divides, than create combination using the found list.
+        //return sum(all divisors)==num
+        if(num==1)return false;
+        List<Integer> myDivisor = new ArrayList<Integer>();
+        myDivisor.add(1);
+        int total=1;
+        for(int i=2;i<Math.sqrt(num);i++){
+            if(num%i==0){
+                total+=i;
+                if(!(num/i==i)) total+=num/i;
+            }
+        }
+        
+        return total==num;
+    }
+}class Solution {
+    public boolean checkPerfectNumber(int num) {
         
     }
 }
