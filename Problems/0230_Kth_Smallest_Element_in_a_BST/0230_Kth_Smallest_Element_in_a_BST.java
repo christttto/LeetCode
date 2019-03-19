f2e680da3f09a4b522a924013e3535b663e0fbf2
Kris <christttto@berkeley.edu>
2019-03-18 20:55:43 -0700

    0230 works with answer

--- Problems/0230_Kth_Smallest_Element_in_a_BST/0230_Kth_Smallest_Element_in_a_BST.java
+++ Problems/0230_Kth_Smallest_Element_in_a_BST/0230_Kth_Smallest_Element_in_a_BST.java
@@ -9,6 +9,29 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        
+        //bst
+        //helper function to iterate
+        //create priority queue and have k elements
+        //or even, find the root of the left most leaf
+        //dfs k times
+        //assign local value to root,
+        //execute k times where it would return value of the next lowest
+        int i = 0;
+        TreeNode myNode = root;
+        Stack<TreeNode> myStack = new Stack<TreeNode>();
+
+        while(!myStack.isEmpty()||myNode!=null){
+            if(myNode!=null){
+            	myStack.push(myNode);
+            	myNode = myNode.left;
+            }
+            else{
+                TreeNode minNode = myStack.pop();
+                i++;
+                if(i==k) return minNode.val;
+                myNode = minNode.right;
+            }
+        }
+        return root.val;
     }
 }
