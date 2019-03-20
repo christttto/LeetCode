3c6ef8260f73a84d013da57a69e2320795c518d5
Kris <christttto@berkeley.edu>
2019-03-19 19:36:56 -0700

    0002_redo

--- Problems/0002_Add_Two_Numbers/0002_Add_Two_Numbers.java
+++ Problems/0002_Add_Two_Numbers/0002_Add_Two_Numbers.java
@@ -7,45 +7,44 @@
  * }
  */
 class Solution {
+    //create helperfunction for carry, always consider the carry.
+    //if null but there is carry, must return new listnode with val=1
     private ListNode addTwoNumbersHelper(ListNode l1, ListNode l2, int carry){
-        if(l1==null&&l2==null){
-            if(carry==1){
-                return new ListNode(1);
-            }
-            else{
-                return null;
-            }
+        if(l1==null&&l2==null){//last digit
+            if(carry==1) return new ListNode(1);
+            return null;
         }
         else{
-            int myCarry=0;
-            int l1Val;
-            int l2Val;
-            if(l1==null){
-                l1Val=0;
+            if(l1==null){//only l2 is remaining
+                if((l2.val+carry)/10==1){//10
+                    ListNode myNode = new ListNode(0);
+                    myNode.next = addTwoNumbersHelper(null, l2.next, 1);
+                    return myNode;
+                }
+                ListNode myNode = new ListNode(l2.val+carry);
+                myNode.next = addTwoNumbersHelper(null,l2.next,0);
+                return myNode;
             }
-            else{
-                l1Val=l1.val;
+            else if(l2==null){//only l1 is remaining
+                if((l1.val+carry)/10==1){//10
+                    ListNode myNode = new ListNode(0);
+                    myNode.next = addTwoNumbersHelper(l1.next, null, 1);
+                    return myNode;
+                }
+                ListNode myNode = new ListNode(l1.val+carry);
+                myNode.next = addTwoNumbersHelper(l1.next,null,0);
+                return myNode;
             }
-            if(l2==null){
-                l2Val=0;
+            else{//both remaining
+                if((l1.val+l2.val+carry)/10==1){//10
+                    ListNode myNode = new ListNode((l1.val+l2.val+carry)%10);
+                    myNode.next = addTwoNumbersHelper(l1.next, l2.next, 1);
+                    return myNode;
+                }
+                ListNode myNode = new ListNode(l1.val+l2.val+carry);
+                myNode.next = addTwoNumbersHelper(l1.next,l2.next,0);
+                return myNode;
             }
-            else{
-                l2Val=l2.val;
-            }
-            if((l1Val+l2Val+carry)>=10){
-                myCarry++;
-            }
-            ListNode myNode = new ListNode((l1Val+l2Val+carry)%10);
-            if(l1==null){
-                myNode.next = addTwoNumbersHelper(null,l2.next,myCarry);
-            }
-            else if(l2==null){
-                myNode.next = addTwoNumbersHelper(l1.next,null,myCarry);
-            }
-            else{
-                myNode.next = addTwoNumbersHelper(l1.next,l2.next,myCarry);
-            }
-            return myNode;
         }
     }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
