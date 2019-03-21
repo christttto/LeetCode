97046537c8b3477ffb8f1efe8b690bea5fa3a770
Kris <christttto@berkeley.edu>
2019-03-21 12:38:20 -0700

    0605

--- Problems/0605_Can_Place_Flowers/0605_Can_Place_Flowers.java
+++ Problems/0605_Can_Place_Flowers/0605_Can_Place_Flowers.java
@@ -8,20 +8,24 @@ class Solution {
         //check only for the edge
         //
         //rule out length1 case
-        if(flowerbed.length==1){
+        if(n==0||flowerbed.length==1){
             if(n==0||(flowerbed[0]==0&&n<=1)) return true;
             return false;
         }
         for(int i=0;i<flowerbed.length;i++){//does not take care of edges
-            if(i==0&&flowerbed[i+1]==0){//left most edge
-                if(--n==0) return true;
-                i++;
+            if(i==0){//left most edge
+                if(flowerbed[i+1]==0&&flowerbed[i]==0){
+                    if(--n==0) return true;
+                    i++;
+                }
             }
-            else if(i==flowerbed.length-1&&flowerbed[i-1]==0){//right most edge
-                if(--n==0) return true;
+            else if(i==flowerbed.length-1){//right most edge
+                if(flowerbed[i-1]==0&&flowerbed[i]==0){
+                    if(--n==0) return true;    
+                }
             }
             else{
-                if(flowerbed[i-1]==0&&flowerbed[i+1]==0){
+                if(flowerbed[i]==0&&flowerbed[i-1]==0&&flowerbed[i+1]==0){
                     if(--n==0) return true;
                     i++;
                 }
