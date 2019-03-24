bf9a396bba7b1a11b9db6a092c201572eb2ae847
Kris <christttto@berkeley.edu>
2019-03-24 10:35:16 -0700

    0611 works

--- Problems/0611_Valid_Triangle_Number/0611_Valid_Triangle_Number.java
+++ Problems/0611_Valid_Triangle_Number/0611_Valid_Triangle_Number.java
@@ -4,18 +4,20 @@ class Solution {
         //from back, go forward
         //the condition for valid trangle side is to have sum of 2,3 be lower than the first.
         //from the far back. if you hit one false, anything beyond that will be too small.
+        Arrays.sort(nums);
         int validTriangle=0;
         for(int first=nums.length-1;first>1;first--){//the largest. goes all the way, but can be changed later
-            System.out.println("first: "+first);
+            
             int second=first-1;
             int third=first-2;
             
-            while(nums[second]+nums[third]>nums[first]){
-                while(nums[second]+nums[third]>nums[first]){
+            while(third>=0&&nums[third]>0&&nums[second]+nums[third]>nums[first]){
+                
+                while(third>=0&&nums[third]>0&&nums[second]+nums[third]>nums[first]){
                     validTriangle++;
                     third--;
                 }
-                third=--second;
+                third=--second-1;
             }
         }
         return validTriangle;
