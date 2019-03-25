f31a9ce71c573584402ce011eaaab307fb63cb50
Kris <christttto@berkeley.edu>
2019-03-25 00:46:31 -0700

    0645 continue

--- Problems/0645_Set_Mismatch/0645_Set_Mismatch.java
+++ Problems/0645_Set_Mismatch/0645_Set_Mismatch.java
@@ -1,15 +1,18 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        //could sort once and do bst
-        //nlogn+logn
+        //use set to find the duplicate
+        //finish making the set of nums-duplicate
+        //if we know what number was repeated, the second repeat was the right position.
         //
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(i+1!=nums[i]){
-                return new int[]{nums[i],i+1};
-            }
+        Set<Integer> mySet = new HashSet<Integer>();
+        int myRepeat = 0;
+        for(int i=0;i<nums.length-1;i++){
+            if(!mySet.add(nums[i])) myRepeat = nums[i];
         }
-        return null;
+        if(myRepeat==0)return new int[]{nums[nums.length-1],nums[nums.length-1]+1};//the very last one was repeat
+        int i=1;
+        while(mySet.contains(i++)){}
+        return new int[]{myRepeat,--i};
     }
 
 
