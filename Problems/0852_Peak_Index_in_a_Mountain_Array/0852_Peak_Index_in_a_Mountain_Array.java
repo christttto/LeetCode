0833cf3afbf47fc686cf9787a8cc252f0aa17788
Kris <christttto@berkeley.edu>
2019-04-08 23:00:35 -0700

    0852

---
+++ Problems/0852_Peak_Index_in_a_Mountain_Array/0852_Peak_Index_in_a_Mountain_Array.java
@@ -0,0 +1,8 @@
+class Solution {
+    //so if not a mountain, returns 0. but no worries, it has been abstracted.
+    public int peakIndexInMountainArray(int[] A) {
+        //use bst type of approach
+        //given the 
+        
+    }
+}
