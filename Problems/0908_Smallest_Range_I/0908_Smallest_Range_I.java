f8704acc145736b5b10fface73daaa413dae919b
Kris <christttto@berkeley.edu>
2019-04-11 12:35:15 -0700

    0908 done

--- Problems/0908_Smallest_Range_I/0908_Smallest_Range_I.java
+++ Problems/0908_Smallest_Range_I/0908_Smallest_Range_I.java
@@ -1,5 +1,13 @@
 class Solution {
     public int smallestRangeI(int[] A, int K) {
-        
+        if(A.length==1) return 0;
+        int myMin = Integer.MAX_VALUE;
+        int myMax = Integer.MIN_VALUE;
+        for(int i:A){
+            if(i>myMax)myMax=i;
+            if(i<myMin)myMin=i;
+        }
+        if((myMax-myMin)<=(K*2))return 0;
+        else return ((myMax-myMin)-(K*2));
     }
 }
