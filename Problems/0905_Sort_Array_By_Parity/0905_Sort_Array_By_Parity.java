2abe6e679d3c9ccbc1a4c714f264e7d77eaeea29
Kris <christttto@berkeley.edu>
2019-04-11 12:12:57 -0700

    works

--- Problems/0905_Sort_Array_By_Parity/0905_Sort_Array_By_Parity.java
+++ Problems/0905_Sort_Array_By_Parity/0905_Sort_Array_By_Parity.java
@@ -1,5 +1,16 @@
 class Solution {
     public int[] sortArrayByParity(int[] A) {
-        
+        ArrayList<Integer> evenList = new ArrayList<Integer>();
+        ArrayList<Integer> oddList = new ArrayList<Integer>();
+        for(int n:A){
+            if(n%2==0){
+                evenList.add(n);
+            }
+            else{
+                oddList.add(n);
+            }
+        }
+        evenList.addAll(oddList);
+        return evenList.stream().mapToInt(i -> i).toArray();
     }
 }
