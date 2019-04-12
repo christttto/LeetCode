b6c94d463dad5e53e13511b764e99e3ea03d79f4
Kris <christttto@berkeley.edu>
2019-04-11 21:53:26 -0700

    0290

--- Problems/0290_Word_Pattern/0290_Word_Pattern.java
+++ Problems/0290_Word_Pattern/0290_Word_Pattern.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean wordPattern(String pattern, String str) {
-        
+        //make hashmap of str's char and relevent pattern
+        HashMap<Character,String> myPattern = new HashMap<Character,String>();
+        String[] strArray = str.split(" ");
+        char[] patternArray = pattern.toCharArray();
+        if(strArray.length!=patternArray.length)return false;
+        for(int i=0;i<strArray.length;i++){
+            if(myPattern.containsKey(patternArray[i])){
+                System.out.println(myPattern.get(patternArray[i])!=strArray[i]);
+                if(myPattern.get(patternArray[i])!=strArray[i]) return false;
+            }
+            else{
+                myPattern.put(patternArray[i],strArray[i]);
+            }
+        }
+        return true;
     }
 }
