039150135b6fe58cecb42a4dde8cfdbb35208508
Kris <christttto@berkeley.edu>
2019-04-14 02:29:15 -0700

    not sure why it does not work

--- Problems/0290_Word_Pattern/0290_Word_Pattern.java
+++ Problems/0290_Word_Pattern/0290_Word_Pattern.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean wordPattern(String pattern, String str) {
-        //make hashmap of str's char and relevent pattern
-        HashMap<Character,String> myPattern = new HashMap<Character,String>();
-        String[] strArray = str.split(" ");
-        char[] patternArray = pattern.toCharArray();
-        if(strArray.length!=patternArray.length)return false;
-        for(int i=0;i<strArray.length;i++){
-            if(myPattern.containsKey(patternArray[i])){
-                System.out.println(myPattern.get(patternArray[i])!=strArray[i]);
-                if(myPattern.get(patternArray[i])!=strArray[i]) return false;
-            }
-            else{
-                myPattern.put(patternArray[i],strArray[i]);
-            }
+        //create stringList
+        String[] strList = str.split(" ");
+        //if length of pattern != str.length, return false
+        if(pattern.length()!=strList.length)return false;
+        //make hashmap
+        HashMap myMap = new HashMap();
+        for(int i=0; i<pattern.length();i++){
+            //
+            if (myMap.put(pattern.charAt(i), i) != myMap.put(strList[i], i)) return false;
+            
         }
         return true;
     }
