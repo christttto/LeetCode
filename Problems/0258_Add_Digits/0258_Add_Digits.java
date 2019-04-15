87aab9559f7f8c5106365656f94c6a5828d10953
Kris <christttto@berkeley.edu>
2019-04-15 14:45:42 -0700

    0258 optimized

--- Problems/0258_Add_Digits/0258_Add_Digits.java
+++ Problems/0258_Add_Digits/0258_Add_Digits.java
@@ -1,11 +1,7 @@
 class Solution {
     public int addDigits(int num) {
-        if(num/10==0) return num;
-        int myNum=0;
-        while(num>0){
-            myNum+=num%10;
-            num=num/10;
-        }
-        return addDigits(myNum);
+        if(num==0)return 0;
+        if(num%9==0)return 9;
+        return num%9;
     }
 }
