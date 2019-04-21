417005336a20087dd70c703f69781cd2c3c1ec3a
Kris <christttto@berkeley.edu>
2019-04-21 00:59:08 -0700

    0206

--- Problems/0206_Reverse_Linked_List/0206_Reverse_Linked_List.java
+++ Problems/0206_Reverse_Linked_List/0206_Reverse_Linked_List.java
@@ -8,6 +8,20 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        if(head==null)return null;
+        ArrayList<Integer> myNodes = new ArrayList<Integer>();
+        while(head!=null){
+            myNodes.add(head.val);
+            head = head.next;
+        }
+        ListNode reversedNode = new ListNode(myNodes.get(myNodes.size()-1));
+        ListNode nodeHead = reversedNode;
+        for(int i=myNodes.size()-2;i>=0;i--){
+            ListNode nextNode = new ListNode(myNodes.get(i));
+            nodeHead.next = nextNode;
+            nodeHead = nextNode;
+        }
+        return reversedNode;
         
     }
 }
