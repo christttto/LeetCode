6aae8b0f371335ccbb78bc15cd2a8dc404ad2af7
Kris <christttto@berkeley.edu>
2019-04-22 18:58:22 -0700

    0121 re-did

--- Problems/0121_Best_Time_to_Buy_and_Sell_Stock/0121_Best_Time_to_Buy_and_Sell_Stock.java
+++ Problems/0121_Best_Time_to_Buy_and_Sell_Stock/0121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -1,37 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //best case: buy from the lowest and sell at highest
-        //basic method: compare every permutation
-        //n to n+1,2,3...
-        //keep the biggest distance
-        //can I assume?
-        
-        //so this is the base case with worse effeciency.
-        //so with the lowest so far, if there is something even lower, replace the lower
-        //if higher than lower, check if the higher will be high enough for profit
-        //if neither, just move on
-        /*
-        if(prices.length<2){return 0;}
-        int buy;
-        int sell;
-        int profit=0;
-        for(int i=0;i<prices.length-1;i++){
-            buy = prices[i];
-            for(int j=i;j<prices.length;j++){
-                sell = prices[j];
-                if((sell-buy)>profit){
-                    profit = sell-buy;
-                }
+        if(prices.length<2)return 0;
+        int sell=prices[prices.length-1];
+        int profit = 0;
+        for(int i=prices.length-2;i>=0;i--){
+            if(sell<prices[i])sell=prices[i];
+            else{
+                if(sell-prices[i]>profit)profit=sell-prices[i];    
             }
-        }
-        return profit;
-        */
-        if(prices.length<2){return 0;}
-        int minimum=Integer.MAX_VALUE;
-        int profit=0;
-        for(int i=0;i<prices.length;i++){
-            if(minimum>prices[i]){minimum=prices[i];}
-            else if(prices[i]-minimum>profit){profit=prices[i]-minimum;}
         }
         return profit;
     }
