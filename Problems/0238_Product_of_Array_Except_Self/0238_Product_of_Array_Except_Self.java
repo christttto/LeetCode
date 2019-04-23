135b68d4589594d336c0af4ce32327788fd23e99
Kris <christttto@berkeley.edu>
2019-04-22 19:11:01 -0700

    0238

--- Problems/0238_Product_of_Array_Except_Self/0238_Product_of_Array_Except_Self.java
+++ Problems/0238_Product_of_Array_Except_Self/0238_Product_of_Array_Except_Self.java
@@ -1,5 +1,15 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        
+        int[] myNums = new int[nums.length];
+        myNums[0]=1;
+        for(int i=1;i<myNums.length;i++){
+            myNums[i]=myNums[i-1]*nums[i-1];
+        }
+        int next=1;
+        for(int i=myNums.length-1;i>=0;i--){
+            myNums[i]*=next;
+            next*=nums[i];
+        }
+        return myNums;
     }
 }
