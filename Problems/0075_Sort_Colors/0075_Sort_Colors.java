c5efe3c9c356172c691a12ad8f608c1a4af77013
Kris <christttto@berkeley.edu>
2019-04-23 21:40:31 -0700

    0075

--- Problems/0075_Sort_Colors/0075_Sort_Colors.java
+++ Problems/0075_Sort_Colors/0075_Sort_Colors.java
@@ -1,5 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        
+        for(int i=0,j=0,k=nums.length-1;i<=k;i++){
+            //since there are only 0,1,2
+                //if 0, swap with ith
+                //if 1, leave it as is
+                //if 2, send to back
+            if(nums[i]==0){
+                nums[i]=nums[j];
+                nums[j]=0;
+                j++;
+                
+            }
+            else if(nums[i]==2){
+                //swap with what was at k
+                nums[i]=nums[k];
+                nums[k--]=2;
+                i--;
+            }
+            else{
+                
+            }
+            
+        }
     }
 }
