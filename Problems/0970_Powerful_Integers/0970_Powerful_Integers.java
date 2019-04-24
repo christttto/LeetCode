af29c850bb3e56783947b1542f2bef252ca3453b
Kris <christttto@berkeley.edu>
2019-04-24 13:10:36 -0700

    0970

--- Problems/0970_Powerful_Integers/0970_Powerful_Integers.java
+++ Problems/0970_Powerful_Integers/0970_Powerful_Integers.java
@@ -1,5 +1,13 @@
 class Solution {
     public List<Integer> powerfulIntegers(int x, int y, int bound) {
-        
+        Set<Integer> outputSet = new HashSet<Integer>();
+        for(int i=0;Math.pow(x,i)<=bound;i++){
+            for(int j=0;Math.pow(x,i)+Math.pow(y,j)<=bound;j++){
+                outputSet.add((int)(Math.pow(x,i)+Math.pow(y,j)));
+                if(y==1)break;
+            }
+            if(x==1)break;
+        }
+        return outputSet.stream().collect(Collectors.toList());
     }
 }
