e39e6e650161f93aec2a220db069a55489409cdf
Kris <christttto@berkeley.edu>
2019-04-23 22:18:06 -0700

    0445

---
+++ Problems/0445_Add_Two_Numbers_II/0445_Add_Two_Numbers_II.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //memory heavy, but make two lists, than iterate
+        //O(3n);
+        ArrayList<Integer>l1List=new ArrayList<Integer>();
+    }
+}
