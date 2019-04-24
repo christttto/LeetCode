c5a298570214ab2cb45fb2bec42cbfc97c0330e4
Kris <christttto@berkeley.edu>
2019-04-23 22:51:54 -0700

    0485

--- Problems/0485_Max_Consecutive_Ones/0485_Max_Consecutive_Ones.java
+++ Problems/0485_Max_Consecutive_Ones/0485_Max_Consecutive_Ones.java
@@ -1,5 +1,19 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        
+        int max=0;
+        int localMax=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                localMax=0;
+            }
+            else{
+                localMax++;
+                if(max<localMax){
+                    max=localMax;
+                }    
+            }
+            
+        }
+        return max;
     }
 }
