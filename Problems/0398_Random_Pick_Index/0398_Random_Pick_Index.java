bbeb342e40342c0089c22dcb24b1ca0c1b3d0cc5
Kris <christttto@berkeley.edu>
2019-04-24 17:45:57 -0700

    0398

--- Problems/0398_Random_Pick_Index/0398_Random_Pick_Index.java
+++ Problems/0398_Random_Pick_Index/0398_Random_Pick_Index.java
@@ -1,11 +1,23 @@
 class Solution {
-
+    HashMap<Integer,ArrayList> locationMap;
     public Solution(int[] nums) {
-        
+        locationMap = new HashMap<Integer,ArrayList>();
+        for(int i=0;i<nums.length;i++){
+            if(locationMap.containsKey(nums[i])){
+                locationMap.get(nums[i]).add(i);
+            }
+            else{
+                ArrayList<Integer> myList = new ArrayList<Integer>();
+                myList.add(i);
+                locationMap.put(nums[i],myList);
+            }
+        }
     }
     
     public int pick(int target) {
-        
+        ArrayList myLocation=locationMap.get(target);
+        Random rn = new Random();
+        return (int)myLocation.get(rn.nextInt(myLocation.size()));
     }
 }
 
