7d0b05a0303015f1028eb3bf9df10111b83f5ced
Kris <christttto@berkeley.edu>
2019-04-24 19:08:27 -0700

    readme

--- Problems/0841_Keys_and_Rooms/0841_Keys_and_Rooms.java
+++ Problems/0841_Keys_and_Rooms/0841_Keys_and_Rooms.java
@@ -1,5 +1,27 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        
+        Queue<Integer> keyQ = new LinkedList<Integer>();
+        Set<Integer> keySet = new HashSet<Integer>();
+        keySet.add(0);
+        for(int i=0;i<rooms.get(0).size();i++){
+            System.out.println("firstkey"+rooms.get(0).get(i));
+            keyQ.add(rooms.get(0).get(i));
+            keySet.add(rooms.get(0).get(i));
+        }
+        while(!keyQ.isEmpty()){
+            int visitingRoom = keyQ.remove();
+            for(int i=0;i<rooms.get(visitingRoom).size();i++){
+                System.out.println("room im in"+rooms.get(visitingRoom));
+                if(keySet.add(rooms.get(visitingRoom).get(i))){
+                    System.out.println(rooms.get(visitingRoom).get(i));
+                    keyQ.add(rooms.get(visitingRoom).get(i));
+                }
+            }
+        }
+        for(int i=0;i<rooms.size();i++){
+            System.out.println("checking"+i);
+            if(!keySet.contains(i)) return false;
+        }
+        return true;
     }
 }
