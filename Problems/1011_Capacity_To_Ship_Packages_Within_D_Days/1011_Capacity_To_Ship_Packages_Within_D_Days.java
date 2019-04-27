f8ee160edd8033bcb96e6ffb1d714c26106e3f1c
Kris <christttto@berkeley.edu>
2019-04-26 23:07:16 -0700

    1011

--- Problems/1011_Capacity_To_Ship_Packages_Within_D_Days/1011_Capacity_To_Ship_Packages_Within_D_Days.java
+++ Problems/1011_Capacity_To_Ship_Packages_Within_D_Days/1011_Capacity_To_Ship_Packages_Within_D_Days.java
@@ -1,29 +1,30 @@
 class Solution {
-    private boolean shipHelper(int [] W,int D, int C){
-        //did my cargo fill up
-        //did I go over my day limit
-        int myD=0;
-        int myCargo;
-        int wCounter=0;
-        while(myD<D){
-            myCargo=0;
-            while(myCargo<C){
-                if(wCounter<W.length){
-                    
-                }
-            }
-            myD++;
-        }
-        return false;
-    }
+
     public int shipWithinDays(int[] weights, int D) {
         //variation of bs
-        int myMax=Integer.MIN_VALUE;
-        int totalWeight=0;
+        int left=Integer.MIN_VALUE;
+        int right=0;
+        //left is myMax, the right will be totalWeights to create my initial boundary
         for(int i:weights){
-            totalWeight+=i;
-            myMax= myMax<i ? i : myMax;
+            right+=i;
+            left= left<i ? i : left;
         }
-        
+        //binary search with 
+        while(left<right){//so while left bound is still smaller than the right, because it will keep on shrinking
+            int middle=(left+right)/2;//this will become either the left or the right
+            int need=1;
+            int current=0;//current weight 
+            for(int i:weights){
+                if(current+i>middle){//if the weight added goes over my capacity, automatically starts the next day
+                    need++;
+                    current=0;
+                }
+                current+=i;
+            }
+            //this is the binary section
+            if(need>D) left=middle+1;//myCapacity was too small, need to go bigger range
+            else right=middle;//myCapacity was too big, need to go smaller range
+        }
+        return left;
     }
 }
