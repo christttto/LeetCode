9b10e84453f8369f2b33c3b1cef6aa51562993a1
Kris <christttto@berkeley.edu>
2019-04-27 14:03:41 -0700

    0322

--- Problems/0322_Coin_Change/0322_Coin_Change.java
+++ Problems/0322_Coin_Change/0322_Coin_Change.java
@@ -1,5 +1,16 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        //sort the array to use the biggest first
+        Arrays.sort(coins);//nlogn
+        int numOfCoins=0;
+        for(int i=coins.length-1;i>=0;i--){
+            if(amount==0)break;
+            numOfCoins+=amount/coins[i];
+            amount=amount%coins[i];
+        }
+        if(amount==0) return numOfCoins;
+        else return -1;
+        
         
     }
 }
