3160c3f735777aa0f5e4b4de551524553fe37f9e
Kris <christttto@berkeley.edu>
2019-04-26 22:35:13 -0700

    1011

--- Problems/1011_Capacity_To_Ship_Packages_Within_D_Days/1011_Capacity_To_Ship_Packages_Within_D_Days.java
+++ Problems/1011_Capacity_To_Ship_Packages_Within_D_Days/1011_Capacity_To_Ship_Packages_Within_D_Days.java
@@ -1,5 +1,22 @@
 class Solution {
+    private boolean shipHelper(int [] W,int D, int C){
+        
+        for(int i=0;i<D;i++){
+            int myCapacity=0;
+            while(myCapacity<C){
+                
+            }
+        }
+        return false;
+    }
     public int shipWithinDays(int[] weights, int D) {
+        //variation of bs
+        int myMax=Integer.MIN_VALUE;
+        int totalWeight=0;
+        for(int i:weights){
+            totalWeight+=i;
+            myMax= myMax<i ? i : myMax;
+        }
         
     }
 }
