5a84f924914248906e89fa72ff5a210cc848864c
Kris <christttto@berkeley.edu>
2019-04-27 16:24:11 -0700

    0081

--- Problems/0081_Search_in_Rotated_Sorted_Array_II/0081_Search_in_Rotated_Sorted_Array_II.java
+++ Problems/0081_Search_in_Rotated_Sorted_Array_II/0081_Search_in_Rotated_Sorted_Array_II.java
@@ -1,5 +1,26 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int middle=(left+right)/2;
+            if(nums[middle]==target)return true;
+            if(nums[left]<nums[middle]){//left side is normal
+                //check if left side has the target, if not use the right side
+                if(nums[left]<=target&&target<nums[middle])right=middle;
+                else left=middle;
+            }
+            else if(nums[left]>nums[middle]){//right side is normal
+                //check if right side as the target, if not use the left side
+                if(nums[middle]<target&&nums[left]>target) left=middle;
+                else right=middle;
+            }
+            else{
+                left++;
+            }
+            // else if(middle<target)right = middle-1;
+            // else left=middle+1;
+        }
+        return false;
     }
 }
