a21fef4a67b8d4ddd2ecf97d915e690e1aad3130
Kris <christttto@berkeley.edu>
2019-04-28 21:57:34 -0700

    1021

---
+++ Problems/1021_Remove_Outermost_Parentheses/1021_Remove_Outermost_Parentheses.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String removeOuterParentheses(String S) {
+        if(S.length()==0)return "";
+        Stack<Character> sStack = new Stack<Character>();
+        StringBuilder resultS = new StringBuilder();
+        for(int i=0;i<S.length()-1;i++){
+            
+            
+        }
+        return result.toString();
+    }
+}
