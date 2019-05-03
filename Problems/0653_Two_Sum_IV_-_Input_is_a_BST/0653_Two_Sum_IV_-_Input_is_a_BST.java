d1d97a9cec413c9aedc5a2718e03a898fe34096b
Kris <christttto@berkeley.edu>
2019-05-03 16:09:40 -0700

    0653

--- Problems/0653_Two_Sum_IV_-_Input_is_a_BST/0653_Two_Sum_IV_-_Input_is_a_BST.java
+++ Problems/0653_Two_Sum_IV_-_Input_is_a_BST/0653_Two_Sum_IV_-_Input_is_a_BST.java
@@ -1,14 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        
+        if(root==null)return false;
+        Queue<TreeNode> myQ=new LinkedList<>();
+        Set<Integer> myS=new HashSet<Integer>();
+        myQ.add(root);
+        while(!myQ.isEmpty()){
+            TreeNode myN=myQ.remove();
+            int myV=myN.val;
+            if(myS.contains(k-myV))return true;
+            else myS.add(myV);
+            if(myN.left!=null)myQ.add(myN.left);
+            if(myN.right!=null)myQ.add(myN.right);
+        }
+        return false;
     }
 }
