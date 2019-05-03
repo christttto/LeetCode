065d9ab3df201870abc868b2cb077092c3f77335
Kris <christttto@berkeley.edu>
2019-05-03 00:41:36 -0700

    0367

--- Problems/0738_Monotone_Increasing_Digits/0738_Monotone_Increasing_Digit.java
+++ Problems/0738_Monotone_Increasing_Digits/0738_Monotone_Increasing_Digit.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        System.out.println("testing"+num);
         if(num<0||num==2)return false;
         if(num<=1||num==4)return true;
         int left=0,right=num/2;
         while(left<=right){
             int middle=(right+left)/2;
-            System.out.println(middle);
             if(Math.pow(middle,2)==num)return true;
             else if(Math.pow(middle,2)>num){
                 right=middle-1;
