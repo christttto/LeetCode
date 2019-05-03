938e61dd6cc7f04a016c500d2c32075971dec2fe
Kris <christttto@berkeley.edu>
2019-05-03 00:40:51 -0700

    0367

--- Problems/0738_Monotone_Increasing_Digits/0738_Monotone_Increasing_Digit.java
+++ Problems/0738_Monotone_Increasing_Digits/0738_Monotone_Increasing_Digit.java
@@ -1,18 +1,21 @@
 class Solution {
-    private boolean isMonotone(int N){
-        int myN=N;
-        while(myN>9){
-            int myD = myN%10;
-            myD = myD/10;
-            if(myN%10<=myD)return false;
+    public boolean isPerfectSquare(int num) {
+        System.out.println("testing"+num);
+        if(num<0||num==2)return false;
+        if(num<=1||num==4)return true;
+        int left=0,right=num/2;
+        while(left<=right){
+            int middle=(right+left)/2;
+            System.out.println(middle);
+            if(Math.pow(middle,2)==num)return true;
+            else if(Math.pow(middle,2)>num){
+                right=middle-1;
+            }
+            else{//Math.pow(middle,2)<num
+                left=middle+1;
+            }
         }
-        return true;
-    }
-    public int monotoneIncreasingDigits(int N) {
-        if(N<10)return N;
-        while(!isMonotone(N)){
-            //modify
-        }
-        return N;
+        return false;
+        
     }
 }
