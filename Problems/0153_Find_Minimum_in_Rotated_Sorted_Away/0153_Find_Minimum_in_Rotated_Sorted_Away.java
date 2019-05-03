108757f18256e035fed6bee35f3f1641603d2a0f
Kris <christttto@berkeley.edu>
2019-05-03 14:37:54 -0700

    0153

--- Problems/0153_Find_Minimum_in_Rotated_Sorted_Away/0153_Find_Minimum_in_Rotated_Sorted_Away.java
+++ Problems/0153_Find_Minimum_in_Rotated_Sorted_Away/0153_Find_Minimum_in_Rotated_Sorted_Away.java
@@ -1,5 +1,20 @@
 class Solution {
     public int findMin(int[] nums) {
-        
+        if(nums.length==1)return nums[0];
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int middle=(left+right)/2;
+            if(middle>0&&nums[middle-1]>nums[middle]){//the smallest is on my right
+                return nums[middle];
+            }
+            else if(nums[left]<=nums[middle]&&nums[middle]>nums[right]){
+                left=middle+1;
+            }
+            else{
+                right=middle-1;
+            }
+        }
+        return nums[left];
     }
 }
