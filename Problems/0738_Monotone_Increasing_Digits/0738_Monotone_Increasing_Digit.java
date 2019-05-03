29fcee02035652d677fba0162f72c2e33c1c5a5b
Kris <christttto@berkeley.edu>
2019-05-02 22:36:30 -0700

    0738

--- Problems/0738_Monotone_Increasing_Digits/0738_Monotone_Increasing_Digit.java
+++ Problems/0738_Monotone_Increasing_Digits/0738_Monotone_Increasing_Digit.java
@@ -1,6 +1,18 @@
 class Solution {
+    private boolean isMonotone(int N){
+        int myN=N;
+        while(myN>9){
+            int myD = myN%10;
+            myD = myD/10;
+            if(myN%10<=myD)return false;
+        }
+        return true;
+    }
     public int monotoneIncreasingDigits(int N) {
         if(N<10)return N;
-        
+        while(!isMonotone(N)){
+            //modify
+        }
+        return N;
     }
 }
