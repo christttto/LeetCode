d1b2f0e4354b7195074bfe7c2fdfdb086acd6577
Kris <christttto@berkeley.edu>
2019-05-05 23:33:49 -0700

    0951

---
+++ Problems/0951_Flip_Equivalent_Binary_Trees/0951_Flip_Equivalent_Binary_Trees.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if(root1==null&&root2==null){
+            
+        }
+    }
+}
