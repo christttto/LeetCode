3c0e47754629c6076d807308d8cfa17ff59d70e5
Kris <christttto@berkeley.edu>
2019-05-06 14:40:52 -0700

    0956

---
+++ Problems/0956_Univalued_Binary_Tree/0956_Univalued_Binary_Tree.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private boolean treeHelper(TreeNode root, int value){
+        if(r)
+    }
+    public boolean isUnivalTree(TreeNode root) {
+        
+    }
+}
