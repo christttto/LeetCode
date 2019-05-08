8230f9fa5450317440445cceb77f107092086fd0
Kris <christttto@berkeley.edu>
2019-05-08 05:26:04 -0700

    0137

--- Problems/0137_Single_Number_II/0137_Single_Number_II.java
+++ Problems/0137_Single_Number_II/0137_Single_Number_II.java
@@ -1,5 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
         HashSet<Integer> myS=new HashSet<Integer>();
+        int firstTotal=0;
+        int secondTotal=0;
+        for(int i:nums){
+            if(myS.contains(i))firstTotal+=i;
+            else {
+                myS.add(i);
+                secondTotal+=i;
+            }
+        }
+        return secondTotal-(firstTotal/2);
     }
 }
