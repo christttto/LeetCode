07004cd745e72e790e515acb544a1fb153d33521
Kris <christttto@berkeley.edu>
2019-05-07 21:30:02 -0700

    0957

--- Problems/0957_Prison_Cells_After_N_Days/0957_Prison_Cells_After_N_Days.java
+++ Problems/0957_Prison_Cells_After_N_Days/0957_Prison_Cells_After_N_Days.java
@@ -1,5 +1,22 @@
 class Solution {
     public int[] prisonAfterNDays(int[] cells, int N) {
-        
+        HashMap<String,Integer> myMap=new HashMap<String,Integer>();
+        while(0<N){
+            int[] myP= new int[cells.length];
+            myMap.put(Arrays.toString(cells),N--);
+            for(int j=0;j<cells.length;j++){
+                if(j==0||j==cells.length-1)myP[j]=0;
+                else{
+                    if((cells[j-1]==0&&cells[j+1]==0)||
+                       (cells[j-1]==1&&cells[j+1]==1))myP[j]=1;
+                    else myP[j]=0;
+                }
+            }
+            cells=myP;
+            if(myMap.containsKey(Arrays.toString(myP))){
+                N%=myMap.get(Arrays.toString(cells))-N;
+            }
+        }
+        return cells;
     }
 }
