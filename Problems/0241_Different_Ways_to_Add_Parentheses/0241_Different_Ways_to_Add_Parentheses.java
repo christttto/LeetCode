8d1ad6b82a140da52124511620f9500d9cbf220c
Kris <christttto@berkeley.edu>
2019-05-14 16:51:34 -0700

    0241

--- Problems/0241_Different_Ways_to_Add_Parentheses/0241_Different_Ways_to_Add_Parentheses.java
+++ Problems/0241_Different_Ways_to_Add_Parentheses/0241_Different_Ways_to_Add_Parentheses.java
@@ -1,5 +1,7 @@
 class Solution {
     public List<Integer> diffWaysToCompute(String input) {
-        
+        //three seperate calls 
+        //when iteration on input is done, add to set
+        //return the set in List
     }
 }
