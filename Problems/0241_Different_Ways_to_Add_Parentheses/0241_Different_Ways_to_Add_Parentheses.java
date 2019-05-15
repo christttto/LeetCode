666ed858764ffa9ea072884ace34e5184afc10f1
Kris <christttto@berkeley.edu>
2019-05-14 17:28:32 -0700

    0241

--- Problems/0241_Different_Ways_to_Add_Parentheses/0241_Different_Ways_to_Add_Parentheses.java
+++ Problems/0241_Different_Ways_to_Add_Parentheses/0241_Different_Ways_to_Add_Parentheses.java
@@ -3,5 +3,27 @@ class Solution {
         //three seperate calls 
         //when iteration on input is done, add to set
         //return the set in List
+        List<Integer> results = new LinkedList<Integer>();
+        for(int i=0;i<input.length();i++){
+            if(input.charAt(i)=='-'||
+               input.charAt(i)=='+'||
+               input.charAt(i)=='*'){
+                String part1 = input.substring(0,i);
+                String part2=input.substring(i+1);
+                List<Integer> part1Result=diffWaysToCompute(part1);
+                List<Integer> part2Result=diffWaysToCompute(part2);
+                for(int p1:part1Result){
+                    for(int p2:part2Result){
+                        int j=0;
+                        if(input.charAt(i)=='+')j=p1+p2;
+                        else if(input.charAt(i)=='-')j=p1-p2;
+                        else j=p1*p2;
+                        results.add(j);
+                    }
+                }
+            }
+        }
+        if(results.size()==0)results.add(Integer.valueOf(input));
+        return results;
     }
 }
