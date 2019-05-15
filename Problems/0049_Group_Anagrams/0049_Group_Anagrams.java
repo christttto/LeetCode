2a1d446d7f1b587a5647a52eee937377bd25860c
Kris <christttto@berkeley.edu>
2019-05-15 02:28:02 -0700

    0049

---
+++ Problems/0049_Group_Anagrams/0049_Group_Anagrams.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List> anagrams = new List<List>();
+        for(String s:strs){
+            HashMap<Integer,Integer> myMap = new HashMap<Integer,Integer>();
+            for(Character c:s){
+                //add number equivalent 
+            }
+        }
+    }
+}
