a080e01dc1743fdbd80f8d6146cfcf0ca5b42e3f
Kris <christttto@berkeley.edu>
2019-05-20 21:17:10 -0700

    0007

--- Problems/0007_Reverse_Integer/0007_Reverse_Integer.java
+++ Problems/0007_Reverse_Integer/0007_Reverse_Integer.java
@@ -1,27 +1,5 @@
 class Solution {
     public int reverse(int x) {
-        StringBuilder stringX = new StringBuilder();
-        int result;
-        if(x<0){
-            stringX.append(String.valueOf(Math.abs(x)));
-            stringX.reverse();
-            try {
-                result = Integer.parseInt(stringX.toString())*-1;
-            }
-            catch (NumberFormatException e) {
-                return 0;
-            }
-            return result;
-        }
-        stringX.append(String.valueOf(x));
-        stringX.reverse();
-        try {
-            result = Integer.parseInt(stringX.toString());
-        }
-        catch (NumberFormatException e) {
-            return 0;
-        }
-        return result;
         
     }
 }
