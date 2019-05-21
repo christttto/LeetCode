ada6222dcc03e0985ddcad219788562c2d266156
Kris <christttto@berkeley.edu>
2019-05-20 21:22:26 -0700

    readme

--- Problems/0561_Array_Partition_I/0561_Array_Partition_I.java
+++ Problems/0561_Array_Partition_I/0561_Array_Partition_I.java
@@ -1,5 +1,13 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
+        if(nums.length==2)return Math.min(nums[0],nums[1]);
+        //sort array
+        Arrays.sort(nums);
+        int sum=0;
+        for(int i=0;i<nums.length;i+=2){
+            sum+=Math.min(nums[i],nums[i+1]);
+        }
+        return sum;
         
     }
 }
